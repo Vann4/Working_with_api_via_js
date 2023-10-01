380fc74fff3c5a0dc4cb72b00189c882c9bed78e
Vann4 <1vetlan1@mail.ru>
2023-10-01 16:55:08 +0300

    Получение данных через API и вывод этих данных в html

--- js/main.js
+++ js/main.js
@@ -5,8 +5,13 @@ fetch(url)
   .then(function(data) {
     let dataArray = data.data
     for (let i=0; i<dataArray.length; i++){
-        let id = dataArray[i];
-        console.log('Request succeeded with JSON response', id);
+        let array_data = dataArray[i]; //Получение данных из массива
+        let str = JSON.stringify(array_data); //Преобразование массива в строку
+
+        let p_tag = document.createElement('p'); //Создание тега p
+        let bodyElement = document.querySelector('body'); //Нахождение HTMLelement`а body
+        p_tag.textContent = `${str}`; //Вставка строки
+        bodyElement.appendChild(p_tag) //Присвоение к body элементу, тега p
     }
   }).catch(function(error) {
     console.log('Request failed', error);
