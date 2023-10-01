36fc7f5385efa4bf0c914f705ca990200535c53e
Vann4 <1vetlan1@mail.ru>
2023-10-01 23:52:39 +0300

    Добавление css файла и таблицы в html

--- js/main.js
+++ js/main.js
@@ -6,12 +6,11 @@ fetch(url)
     let dataArray = data.data
     for (let i=0; i<dataArray.length; i++){
         let array_data = dataArray[i]; //Получение данных из массива
-        let str = JSON.stringify(array_data); //Преобразование массива в строку
 
-        let p_tag = document.createElement('p'); //Создание тега p
-        let bodyElement = document.querySelector('body'); //Нахождение HTMLelement`а body
-        p_tag.textContent = `${str}`; //Вставка строки
-        bodyElement.appendChild(p_tag) //Присвоение к body элементу, тега p
+        let tr_Element = document.createElement('tr');
+        let table_Element = document.querySelector('table');
+        tr_Element.innerHTML = `<td>${array_data.id}</td> <td>${array_data.email}</td> <td>${array_data.first_name}</td> <td>${array_data.last_name}</td> <td>${array_data.avatar}</td>`;
+        table_Element.appendChild(tr_Element)
     }
   }).catch(function(error) {
     console.log('Request failed', error);
